c511723c6b186c197aedd5e235c198100265a80b
shadowcat0202 <fxlimsin@gmail.com>
2022-02-10 16:31:14 +0900

    리스트관련

---
+++ List.py
@@ -0,0 +1,6 @@
+#리스트
+#valuable = [value1, value2, ...] #물론 String형은 'value1'와 같은 방법으로 입력
+shopping_list = ['milk', 'eggs', 'cheese', 'butter', 'cream']
+print(type(shopping_list), shopping_list)
+shopping_list[2] = 'apple'  #항목 변경 가능
+print(type(shopping_list), shopping_list, "->[2]번 리스트 변경")
