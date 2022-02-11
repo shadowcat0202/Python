1bbc9e494807395c6446a2aff9c87774ea2a5c63
shadowcat0202 <fxlimsin@gmail.com>
2022-02-11 15:02:41 +0900

    리스트 내용 추가

--- List.py
+++ List.py
@@ -4,3 +4,13 @@ shopping_list = ['milk', 'eggs', 'cheese', 'butter', 'cream']
 print(type(shopping_list), shopping_list)
 shopping_list[2] = 'apple'  #항목 변경 가능
 print(type(shopping_list), shopping_list, "->[2]번 리스트 변경")
+list_length = len(shopping_list)
+print(list_length)
+
+name = input("이름:")
+age = int(input("나이:"))
+address = input("주소:")
+tall = int(input("키:"))
+
+person = [name, age, address, tall]
+print(person)
