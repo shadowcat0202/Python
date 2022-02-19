a589ee3c6202e5d34e06978ea3e583ea42c9e45a
shadowcat0202 <fxlimsin@gmail.com>
2022-02-19 14:32:50 +0900

    reorganize compatible VScode

---
+++ main.py
@@ -0,0 +1,10 @@
+# import data_type as dt
+# dt.datatype()
+# dt.valuable()
+# del dt
+
+# import operat as op
+# op.calc()
+# op.number_processing_function()
+# op.random_processing_function()
+# del op
